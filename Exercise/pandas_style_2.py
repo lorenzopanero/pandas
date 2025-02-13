07c19699cbe718b2513379a6a35c3a99b57f308f
lorenzopanero <lorenzpaner@gmail.com>
2025-02-13 10:14:17 +0100

    Update pandas_style_2.py

--- Exercise/pandas_style_2.py
+++ Exercise/pandas_style_2.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Casual dataframe with 10 rows and 4 columns
-data = np.random.randint(1, 100, size=(10, 4))  # Numeri casuali tra 1 e 100
+data = np.random.randint(1, 100, size=(10, 4)) 
 df = pd.DataFrame(data, columns=['A', 'B', 'C', 'D'])
 
 # Function to highlight columns
@@ -14,11 +14,11 @@ def highlight_columns(x):
         'C': 'background-color: lightblue',
         'D': 'background-color: lightcoral'
     }
-    return [color.get(col, '') for col in x.index]  # Restituire il colore per ogni colonna
+    return [color.get(col, '') for col in x.index] 
 
 # apply style
-styled_df = df.style.apply(highlight_columns, axis=0)  # axis=0 per colonne
+color_df = df.style.apply(highlight_columns, axis=0)  
 
-print(styled_df)
+print(color_df)
 
 
